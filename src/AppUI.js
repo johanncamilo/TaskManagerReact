c8566a85ab7b01565d5b936535676b5ad4ee4aed
Johann Camilo Belosareos Colmenares <milodevcool@gmail.com>
2023-12-08 21:04:19 -0500

    loading skeleton

--- src/AppUI.js
+++ src/AppUI.js
@@ -4,6 +4,10 @@ import { TodoList } from './TodoList/TodoList'
 import { TodoItem } from './TodoItem/TodoItem'
 import { CreateTodoButton } from './CreateTodoButton/CreateTodoButton'
 
+import { TodosLoading } from './TodosLoading/TodosLoading'
+import { TodosError } from './TodosError/TodosError'
+import { EmptyTodos } from './EmptyTodos/EmptyTodos'
+
 export const AppUI = ({ loading, error, completedTodos, totalTodos, felicitaciones, searchValue, setSearchValue, searchedTodos, checkTodo, deleteTodo }) => {
   return (
     <>
@@ -11,9 +15,15 @@ export const AppUI = ({ loading, error, completedTodos, totalTodos, felicitacion
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {loading && <p>cargando...</p>}
-        {error && <p>desespérate hubo un error!</p>}
-        {!loading && searchedTodos.length === 0 && <p>Crea tu primer TODO</p>}
+        {loading && (
+          <>
+            <TodosLoading />
+            <TodosLoading />
+            <TodosLoading />
+          </>
+        )}
+        {error && <TodosError />}
+        {!loading && searchedTodos.length === 0 && <EmptyTodos />}
 
         {searchedTodos.map(({ text, completed }) => (
           <TodoItem key={text} text={text} completed={completed} onComplete={() => checkTodo(text)} onDelete={() => deleteTodo(text)} />
---
+++ src/TodosError/TodosError.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const TodosError = () => {
+  return <p>desespérate hubo un error!</p>
+}
